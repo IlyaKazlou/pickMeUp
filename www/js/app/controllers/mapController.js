ab8d58156b92b6c3905e6a866872bf50b2221312
Ilya Kazlou <Ilya_Kazlou1@epam.com>
2015-07-10 22:27:00 +0300

    Get Subscribers

--- www/js/app/controllers/mapController.js
+++ www/js/app/controllers/mapController.js
@@ -2,7 +2,9 @@
 function mapController($scope, mapService){
     $scope.init = function () {
         mapService.createMap().then(function (position) {
-            mapService.updateCurrentUserCoords($scope.user.id, position);
+            mapService.updateCurrentUserCoords($scope.user.id, position).then(function () {
+                mapService.addSubscribersToMap($scope.user.id,$scope.user.currentRole);
+            });
         });
     };
 
--- www/js/app/services/mapServiceFactory.js
+++ www/js/app/services/mapServiceFactory.js
@@ -26,6 +26,32 @@ function mapServiceFactory($http, $q, backendSettings){
         return deferred.promise;
     };
 
+    var _getSubscribers = function (id, currentUserRole){
+        var deferred = $q.defer();
+        var request = { "UserId": id, "CurrentUserRole": currentUserRole};
+        $http.post(baseUri + "api/AppApi/GetUserSubscribers", request).success(function (response) {
+            deferred.resolve(response);
+        }).error(function (e){
+            deferred.reject(e);
+        });
+
+        return deferred.promise;
+    };
+
+    var _addSubscribersToMap = function (userId, currentUserRole) {
+        _getSubscribers(userId, currentUserRole).then(function (subscribers){
+            if (subscribers) return;
+            for (var i = 0; i < subscribers.length; i++){
+                var latestPoint = subscribers[i].LatestPoint;
+                if (latestPoint)
+                {
+                    var placemark = new ymaps.Placemark([latestPoint.Latitude, latestPoint.Longitude]);
+                    map.geoObjects.add(placemark);
+                }
+            }
+        });
+    };
+
     var _getCurrentPosition = function () {
         var deferred = $q.defer();
         navigator.geolocation.getCurrentPosition(function (position) {
@@ -45,6 +71,20 @@ function mapServiceFactory($http, $q, backendSettings){
         }).error(function (e){
             deferred.reject(e);
         });
+
+        return deferred.promise;
+    };
+
+    var _getUrlParams = function (params){
+        var str ="?";
+        for (var i = 0; i < params.length; i++){
+            for(var prop in params[i]){
+                str += prop + "=" + params[i][prop];
+            }
+
+            str += (i < params.length - 1) ? "&" : "";
+        }
+        return str;
     };
 
     service.createMap = function () {
@@ -59,6 +99,10 @@ function mapServiceFactory($http, $q, backendSettings){
     };
 
     service.updateCurrentUserCoords = _updateCurrentUserCoords;
+
+    service.getSubscribers = _getSubscribers;
+
+    service.addSubscribersToMap = _addSubscribersToMap;
 
     return service;
 };
